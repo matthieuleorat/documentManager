5f82d62c56aed4bfd397b9dc962e29ef6b039169
Matthieu Leorat <matthieu.leorat@protonmail.com>
2017-12-05 07:21:23 +0100

    Restrict tag selection in documentcreation/edition by current user https://github.com/matthieuleorat/documentManager/issues/8

--- src/Controller/DocumentAdminController.php
+++ src/Controller/DocumentAdminController.php
@@ -9,6 +9,9 @@
 namespace App\Controller;
 
 use App\Entity\Document;
+use App\Entity\User;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class DocumentAdminController extends AdminController
 {
@@ -20,5 +23,31 @@ class DocumentAdminController extends AdminController
         $entity = $easyadmin['item'];
 
         return $this->file($entity->getFile(), $entity->getName());
+    }
+
+    public function createDocumentEntityFormBuilder($entity, $view)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $formBuilder = parent::createEntityFormBuilder($entity, $view);
+
+        // Get tags options
+        $tagsOptions = $formBuilder->get('tags')->getOptions();
+
+        // Add the correct query to filter tags by user
+        $tagsOptions['query_builder'] = function (EntityRepository $er) use ($user) {
+            return $er->createQueryBuilder('t')
+                ->where('t.user = :user')
+                ->setParameter('user', 1);
+        };
+
+        // Reset choice_list and choice_loader
+        unset($tagsOptions['choice_list'], $tagsOptions['choice_loader']);
+
+        // Redefined form children tags
+        $formBuilder->add('tags', EntityType::class, $tagsOptions);
+
+        return $formBuilder;
     }
 }
