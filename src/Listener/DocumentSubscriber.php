a313eb15b4ec7fbb8eab83af8e43d6a577213fc4
Matthieu Leorat <matthieu.leorat@protonmail.com>
2018-01-08 18:36:23 +0100

    Log current logged user instead of document'owner for document download

--- src/Listener/DocumentSubscriber.php
+++ src/Listener/DocumentSubscriber.php
@@ -10,22 +10,28 @@ namespace App\Listener;
 
 use App\DocumentEvents;
 use App\Entity\Document;
+use App\Entity\User;
 use App\Event\DocumentDownloadEvent;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class DocumentSubscriber implements EventSubscriberInterface
 {
     /** @var LoggerInterface  */
     private $logger;
 
+    /** @var User  */
+    private $user;
+
     /**
      * DocumentSubscriber constructor.
      * @param LoggerInterface $logger
      */
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, TokenStorageInterface $tokenStorage)
     {
         $this->logger = $logger;
+        $this->user = $tokenStorage->getToken()->getUser();
     }
 
     /**
@@ -45,6 +51,6 @@ class DocumentSubscriber implements EventSubscriberInterface
     {
         /** @var Document $document */
         $document = $event->getDocument();
-        $this->logger->info('download', ['document' => $document->getId(), 'user' => $document->getUser()->getId()]);
+        $this->logger->info('download', ['document' => $document->getId(), 'user' => $this->user->getId()]);
     }
 }
