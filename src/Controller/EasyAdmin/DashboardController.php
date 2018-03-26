26934d76a87d5617fc40e4d300b2ebe64fe39e20
Matthieu Leorat <matthieu.leorat@protonmail.com>
2018-03-26 10:56:46 +0200

    Clean DashboardController

--- src/Controller/EasyAdmin/DashboardController.php
+++ src/Controller/EasyAdmin/DashboardController.php
@@ -16,34 +16,27 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DashboardController extends AdminController
 {
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function dashboard(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
         $tags = $this->getDoctrine()->getRepository(Tag::class)->findAll();
 
-        $form = $this->createForm(SearchTagsType::class, null);
+        $form = $this->createForm(SearchTagsType::class);
 
         $form->handleRequest($request);
 
+        $selectedTags = [];
+
         if ($form->isSubmitted() && $form->isValid()) {
-
             $selectedTags = $form->get('tags')->getData();
-
-            $documents = $em->getRepository(Document::class)->searchByTags($selectedTags);
-
-            return $this->render('EasyAdmin\Dashboard\dashboard.html.twig', [
-                'form' => $form->createView(),
-                'tags' => $tags,
-                'documents' => $documents,
-            ]);
-
-            return $this->render('EasyAdmin\fragment\tagselection.html.twig', [
-                'form' => $form->createView(),
-            ]);
         }
 
-        $documents = $em->getRepository(Document::class)->searchByTags();
+        $documents = $em->getRepository(Document::class)->searchByTags($selectedTags);
 
         return $this->render('EasyAdmin\Dashboard\dashboard.html.twig', [
             'form' => $form->createView(),
